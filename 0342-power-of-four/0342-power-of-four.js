d3b2d99b920825242638541c435367f09dee6a0c
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-08-15 11:01:38 +0530

    Time: 3 ms (3.98%), Space: 57.4 MB (17.42%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    
+    for(i=0;i<=50;i++){
+        if(4**i==n){
+            return true
+        }
+    }
+    return false
+    
+    
+      
+};
