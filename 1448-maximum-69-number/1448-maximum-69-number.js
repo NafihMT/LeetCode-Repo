4c2f18dd5c1c5fa0527b1f4ed66c0d7c44154c41
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-08-16 11:57:22 +0530

    Time: 0 ms (100%), Space: 53.4 MB (64.5%) - LeetHub

---
+++ 1448-maximum-69-number/1448-maximum-69-number.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+          
+    let str = num.toString().replace("6","9")
+    return parseInt(str)
+};
