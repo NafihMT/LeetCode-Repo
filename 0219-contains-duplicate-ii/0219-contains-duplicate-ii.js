7f3e2b9b816745b9e114dc2e9feb11f5ca0b6633
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-08-18 14:30:35 +0530

    Time: 26 ms (68.67%), Space: 70.3 MB (89.1%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    let set1 = new Set();
+    for(let i=0;i<nums.length;i++){
+        if(set1.has(nums[i])){
+            return true
+        }
+        set1.add(nums[i])
+        if(set1.size > k){
+            set1.delete(nums[i-k])
+        }
+    }
+    return false
+};
