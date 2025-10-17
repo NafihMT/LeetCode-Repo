c4ad2c8de1dd13bf1538ddac581245ddbd152fbe
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-10-17 14:05:42 +0530

    Time: 0 ms (100%), Space: 55.8 MB (20.9%) - LeetHub

--- 3830-find-closest-person/3830-find-closest-person.js
+++ 3830-find-closest-person/3830-find-closest-person.js
@@ -6,15 +6,19 @@
  */
 var findClosest = function (x, y, z) {
 
-    let minimum = Math.min(Math.abs(x - z), Math.abs(y - z))
-    if (minimum === Math.abs(x - z) && Math.abs(x - z) !== Math.abs(y - z) ) {
-        return 1
-    }
-    else if (minimum === Math.abs(y - z) && Math.abs(x - z) !== Math.abs(y - z)) {
-        return 2
-    }
-    else {
-        return 0
-    }
+    // let minimum = Math.min(Math.abs(x - z), Math.abs(y - z))
+    // if (minimum === Math.abs(x - z) && Math.abs(x - z) !== Math.abs(y - z)) {
+    //     return 1
+    // }
+    // else if (minimum === Math.abs(y - z) && Math.abs(x - z) !== Math.abs(y - z)) {
+    //     return 2
+    // }
+    // else {
+    //     return 0
+    // }
+
+    if (Math.abs(x - z) < Math.abs(y - z)) return 1
+    else if (Math.abs(x - z) > Math.abs(y - z)) return 2
+    else return 0
 
 };
