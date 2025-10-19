6d5febb842054a4b1922fef3ebedd6588136f63b
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-10-19 18:53:20 +0530

    Time: 52 ms (61.55%), Space: 55.2 MB (80.71%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.js
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -4,14 +4,11 @@
  */
 var reverse = function (x) {
 
-    let reverse = x.toString().split('').reverse().join('')
-    let reverseNum = parseInt(reverse)
-    if (reverseNum > 2147483647) {
+    let reverse = parseInt(
+        x.toString().split('').reverse().join('')
+        ) * Math.sign(x)
+    if (reverse < Math.pow(-2, 31) || reverse > Math.pow(2, 31) - 1) {
         return 0
     }
-    else {
-
-        return reverseNum * Math.sign(x)
-    }
-
+    return reverse
 };
