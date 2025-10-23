cb042186b5b8e01688ce3288316b61e719dbc081
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-10-23 10:00:19 +0530

    Time: 9 ms (71.01%), Space: 60.6 MB (27.54%) - LeetHub

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var hasSameDigits = function (s) {
+    while (s.length > 2) {
+        let newStr = ''
+        for (let i = 0; i < s.length - 1; i++) {
+            let sum = (Number(s[i]) + Number(s[i + 1])) % 10
+            newStr += sum
+        }
+        s = newStr
+    }
+    return s[0] === s[1]
+};
