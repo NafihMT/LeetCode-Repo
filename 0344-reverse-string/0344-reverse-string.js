31412fbc3f1530e6f32d8b4c046f69a6ed372760
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-11-11 14:53:49 +0530

    Time: 3 ms (13%), Space: 61.4 MB (32.87%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.js
+++ 0344-reverse-string/0344-reverse-string.js
@@ -2,7 +2,10 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-var reverseString = function(s) {
-    return s.reverse();
-    
+var reverseString = function (s) {
+    for (let i = 0; i < Math.floor(s.length / 2); i++) {
+        [s[i], s[s.length - 1 - i]] = [s[s.length - 1 - i], s[i]];
+    }
+
+
 };
