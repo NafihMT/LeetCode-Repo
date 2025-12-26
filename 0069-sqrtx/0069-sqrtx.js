fc0866cf94062fc0fdb5be8aef747e1f39b9d243
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2025-12-26 09:43:17 +0530

    Time: 20 ms (15.58%), Space: 58 MB (5.09%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -2,6 +2,11 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
+var mySqrt = function (x) {
+    let i = 0;
+    while ((i + 1) * (i + 1) <= x) {
+        i++
+    }
+    return i
+
 };
