3ad77e89d77883739721018f43907561f848c63c
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-01-05 16:38:44 +0530

    Time: 15 ms (75.15%), Space: 12.7 MB (11.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -4,11 +4,26 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if(n <= 1):
-            return n
+        # if(n <= 1):
+        #     return n
         
-        a = 0
-        b = 1
-        for i in range(2, n + 1):
-            a , b = b , a + b
-        return b
+        # a = 0
+        # b = 1
+        # for i in range(2, n + 1):
+        #     a , b = b , a + b
+        # return b
+
+        cache = {}
+        def helper(n):
+            if(n == 0):
+                return 0
+            if(n == 1):
+                return 1
+
+            if(n in cache):
+                return cache[n]
+            cache[n] = helper(n - 1) + helper(n - 2)
+        
+            return cache[n]
+            
+        return helper(n)
