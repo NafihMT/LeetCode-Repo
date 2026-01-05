02b50b685877b4840e121f4f102b1f70d37abbd5
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-01-05 16:42:11 +0530

    Time: 964 ms (5.12%), Space: 12.5 MB (11.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -14,16 +14,15 @@ class Solution(object):
         # return b
 
         cache = {}
-        def helper(n):
-            if(n == 0):
-                return 0
-            if(n == 1):
-                return 1
+    
+        if(n == 0):
+            return 0
+        if(n == 1):
+            return 1
 
-            if(n in cache):
-                return cache[n]
-            cache[n] = helper(n - 1) + helper(n - 2)
-        
+        if(n in cache):
             return cache[n]
+        cache[n] = self.fib(n - 1) + self.fib(n - 2)
+        
+        return cache[n]
             
-        return helper(n)
