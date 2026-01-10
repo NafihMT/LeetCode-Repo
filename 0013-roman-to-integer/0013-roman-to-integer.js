e4e0ec96ed13f1bfa57b88099c60f7eeccf42016
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-01-10 15:10:26 +0530

    Time: 3 ms (89.44%), Space: 62 MB (18.92%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function (s) {
+    let roman = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    }
+    let res = 0
+    for (let i = 0; i < s.length; i++) {
+
+        const current = roman[s[i]]
+        const next = roman[s[i + 1]]
+
+        if (next && current < next) {
+            res -= current
+        }
+        else {
+            res += current
+        }
+
+    }
+    return res
+
+};
