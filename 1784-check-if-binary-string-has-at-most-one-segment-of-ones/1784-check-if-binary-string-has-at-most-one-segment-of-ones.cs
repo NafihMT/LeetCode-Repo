36a91b5ae9d6aaee88e4161edae05f7cc80f87c2
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-03-09 12:18:32 +0530

    Time: 0 ms (100%), Space: 40.8 MB (61.92%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cs
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public bool CheckOnesSegment(string s) {
-        return !s.Contains("01");
+        // return !s.Contains("01");
+
+        bool zeroFound = false;
+        for(int i = 1; i < s.Length; i++){
+            if(s[i] == '0'){
+                zeroFound = true;
+            }
+            if(zeroFound && s[i] == '1'){
+                return false;
+            }
+        }
+        return true;
     }
 }
