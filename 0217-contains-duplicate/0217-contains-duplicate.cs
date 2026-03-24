4270b84d019ea9e125d121fdd068e53f8be9254f
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-03-24 11:56:37 +0530

    Time: 13 ms (72.66%), Space: 78.4 MB (6.42%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> hs =new HashSet<int>();
+        for(int i=0; i<nums.Length; i++){
+            if(!hs.Contains(nums[i])){
+                hs.Add(nums[i]);
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
