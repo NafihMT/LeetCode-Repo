daf786a5ce2a38015f894ed631f4415b3a4b211e
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-03-25 15:30:14 +0530

    Time: 4 ms (64.49%), Space: 49.7 MB (24.03%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int,int> dict = new Dictionary<int,int>();
+        for(int i=0; i<nums.Length; i++){
+            int diff = target - nums[i];
+            if(dict.ContainsKey(diff)){
+                return [dict[diff],i];
+            }
+            if(!dict.ContainsKey(nums[i])){
+                dict.Add(nums[i],i);
+            }
+        }
+        return [0];
+    }
+}
