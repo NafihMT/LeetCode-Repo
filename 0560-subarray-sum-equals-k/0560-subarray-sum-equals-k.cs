a708908b7c9bd0f3430d53ac0ec130705d9ac710
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-04-03 10:50:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cs
@@ -1,19 +1,22 @@
 public class Solution {
     public int SubarraySum(int[] nums, int k) {
-        Dictionary<int,int> ht = new Dictionary<int,int>();
-        ht.Add(0,1);
+        Dictionary<int, int> dict = new Dictionary<int, int>();
         int sum = 0;
         int count = 0;
+        dict[0] = 1;
 
-        for(int i=0;i<nums.Length;i++){
+        for(int i=0; i<nums.Length; i++){
             sum += nums[i];
-            if(ht.ContainsKey(sum - k)){
-                count += ht[sum - k];
+            if(dict.ContainsKey(sum - k)){
+                count += dict[sum - k];
             }
-            ht[sum] = ht.GetValueOrDefault(sum,0) + 1;
-        } 
+            dict[sum] = dict.GetValueOrDefault(sum, 0) + 1;
 
-        Console.WriteLine(ht[0]);
+            foreach(var j in dict){
+                Console.WriteLine(j);
+            }
+            Console.WriteLine("\n");
+        }
         return count;
     }
 }
