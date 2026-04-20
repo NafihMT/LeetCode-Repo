5f9275512e8d0336568bd9d9853780c4a3973aa9
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-04-20 12:37:31 +0530

    Time: 0 ms (100%), Space: 42.7 MB (62.75%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.cs
+++ 3340-check-balanced-string/3340-check-balanced-string.cs
@@ -1,17 +1,15 @@
 public class Solution {
     public bool IsBalanced(string num) {
-        int even = 0;
-        int odd = 0;
-        for(int i = 0; i < num.Length; i++){
-            if(i % 2 == 0){
-                even += num[i] - '0';
-            }
-            else{
-                odd += num[i] - '0';
-            }
-        Console.WriteLine(even + " " + odd);
-        }
-        return even == odd;
+        int evenSum = 0, oddSum = 0;
         
+        for (int i = 0; i < num.Length; i++) {
+            if (i % 2 == 0) {
+                evenSum += num[i] - '0';
+            } else {
+                oddSum += num[i] - '0';
+            }
+        }
+        
+        return evenSum == oddSum;
     }
 }
