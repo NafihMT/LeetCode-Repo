4ca7e8fa92ef44ff5390226ee9fff1de0c23ef95
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-06-15 14:50:21 +0530

    Time: 1 ms (43.24%), Space: 44.4 MB (94.59%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.cs
+++ 3024-type-of-triangle/3024-type-of-triangle.cs
@@ -1,8 +1,6 @@
 public class Solution {
     public string TriangleType(int[] nums) {
-        // if(nums[0] + nums[1] <= nums[2] || nums[1] + nums[2] <= nums[0] || nums[0] + nums[2] <= nums[1]){
-        //     return "none";
-        // }
+       
         Array.Sort(nums);
         if(nums[0] + nums[1] <= nums[2]){
             return "none";
