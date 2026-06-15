052da45f5611721b16ca22103be06b075f9f50a2
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-06-15 14:46:47 +0530

    Time: 0 ms (100%), Space: 44.7 MB (59.46%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.cs
+++ 3024-type-of-triangle/3024-type-of-triangle.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public string TriangleType(int[] nums) {
-        if(nums[0] + nums[1] <= nums[2] || nums[1] + nums[2] <= nums[0] || nums[0] + nums[2] <= nums[1]){
+        if(nums[0] + nums[1] <= nums[2] || nums[2] + nums[1] <= nums[0] || nums[0] + nums[2] <= nums[1]){
             return "none";
         }
         if(nums[0]==nums[1] && nums[1]==nums[2]){
