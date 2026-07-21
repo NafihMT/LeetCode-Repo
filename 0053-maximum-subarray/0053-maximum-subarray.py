f202b7589b05ffc0de41f704efcf169a66103561
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-07-21 15:41:27 +0530

    Time: 63 ms (70.8%), Space: 22.1 MB (11.28%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -7,9 +7,10 @@ class Solution(object):
         curr_sum = 0
         max_sum = float('-inf')
         for num in nums:
-            curr_sum = max(num, curr_sum + num)
+            curr_sum = curr_sum + num
             max_sum = max(max_sum, curr_sum)
-            # if(curr_sum < 0):
-            #     curr_sum = 0
+
+            if(curr_sum < 0):
+                curr_sum = 0
         return max_sum 
         
