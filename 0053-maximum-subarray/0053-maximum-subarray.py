661d03a30bab7cfe0ba9d98868304163b4d76618
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-07-22 10:36:32 +0530

    Time: 88 ms (57.12%), Space: 22 MB (11.24%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,10 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        curr_sum = nums[0]
-        max_sum = nums[0]
-        for num in nums[1:]:
+        curr_sum = 0
+        max_sum = float('-inf')
+        for num in nums:
             curr_sum = max(num, curr_sum + num)
-            max_sum = max(max_sum, curr_sum)
-        return max_sum 
-        
+            max_sum = max(curr_sum, max_sum)
+
+        return max_sum
