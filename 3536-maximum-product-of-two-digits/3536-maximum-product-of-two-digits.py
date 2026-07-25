4bd247883bfbf6173fdab3849f236e98c1825194
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-07-25 16:23:41 +0530

    Time: 0 ms (100%), Space: 12.6 MB (0%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.py
@@ -4,8 +4,6 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        # n = [int(d) for d in str(n)]
-        # n.sort()
         m = sorted(str(n))
         return int(m[-1]) * int(m[-2])
         
