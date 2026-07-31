e69d31ca7e852899e893d5ddcab2904b9f8f8a8f
Nafih MT <125077767+NafihMT@users.noreply.github.com>
2026-07-31 15:27:06 +0530

    Time: 20 ms (50.46%), Space: 51.3 MB (14.34%) - LeetHub

---
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int FirstUniqChar(string s) {
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+
+        foreach(char c in s){
+            if(freq.ContainsKey(c))
+                freq[c]++;
+            else
+                freq[c] = 1;
+        }
+        for(int i=0; i<s.Length; i++){
+            if(freq[s[i]] == 1)
+                return i;
+        }
+        return -1;
+    }
+}
